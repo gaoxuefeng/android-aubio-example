7a3106ec814b98b76c7ae013b84d237380f28a97
Gao Xuefeng <1072820194@qq.com>
2020-11-16 17:33:56 +0800

    改为androidx 启用viewBinding

--- app/src/main/java/ffpetrovic/anrdroid_aubio_example/MainActivity.java
+++ app/src/main/java/ffpetrovic/anrdroid_aubio_example/MainActivity.java
@@ -1,56 +1,96 @@
 package ffpetrovic.anrdroid_aubio_example;
 
 import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
-import android.os.Handler;
-import android.os.Looper;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import java.util.Map;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int         sampleRate = 0;
-    private int         bufferSize = 0;
-    private int         readSize = 0;
-    private int         amountRead = 0;
-    private float[]     buffer = null;
-    private short[]     intermediaryBuffer = null;
+    private int sampleRate = 0;
+    private int bufferSize = 0;
+    private int readSize = 0;
+    private int amountRead = 0;
+    private float[] buffer = null;
+    private short[] intermediaryBuffer = null;
 
     /* These variables are used to store pointers to the C objects so JNI can keep track of them */
     public long ptr = 0;
     public long input = 0;
     public long pitch = 0;
 
-    public boolean      isRecording = false;
+    public boolean isRecording = false;
     private AudioRecord audioRecord = null;
     Thread audioThread;
 
+    ActivityResultLauncher<String[]> launcher;
+    private View btPermission;
+    private View btStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btPermission = findViewById(R.id.bt_permission);
+        btStop = findViewById(R.id.bt_stop);
+        btPermission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestPermission();
+            }
+        });
+        btStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopRecord();
+            }
+        });
+        launcher = registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), new ActivityResultCallback<Map<String, Boolean>>() {
+            @Override
+            public void onActivityResult(Map<String, Boolean> result) {
+                if (result.containsValue(true)) {
+                    btPermission.setVisibility(View.GONE);
 
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+                    init();
+                    start();
+                } else {
+                    btPermission.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        requestPermission();
+    }
 
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},
-                    11);
+    private void stopRecord() {
+        btPermission.setVisibility(View.VISIBLE);
+        btStop.setVisibility(View.GONE);
+        if (isRecording) {
+            isRecording = false;
+            if (audioRecord != null) {
+                audioRecord.stop();
+                audioRecord.release();
+                audioRecord = null;
+            }
+            if (audioThread != null) {
+                audioThread.interrupt();
+                audioThread = null;
+            }
 
-
-        } else {
-            init();
-            start();
         }
+    }
 
-
+    private void requestPermission() {
+        launcher.launch(new String[]{Manifest.permission.RECORD_AUDIO});
     }
 
     static {
@@ -65,9 +105,10 @@ public class MainActivity extends AppCompatActivity {
         buffer = new float[readSize];
         intermediaryBuffer = new short[readSize];
     }
-//
+
+    //
     public void start() {
-        if(!isRecording) {
+        if (!isRecording) {
             isRecording = true;
 //        sampleRate = AudioUtils.getSampleRate();
 //        bufferSize = AudioRecord.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_IN_DEFAULT, AudioFormat.ENCODING_PCM_16BIT);
@@ -79,6 +120,7 @@ public class MainActivity extends AppCompatActivity {
                 //Runs off the UI thread
                 @Override
                 public void run() {
+                    System.out.println("线程继续运行");
                     findNote();
                 }
             }, "Tuner Thread");
@@ -87,7 +129,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void findNote() {
-        while (isRecording) {
+        while (isRecording && audioThread != null && !audioThread.isInterrupted()) {
             amountRead = audioRecord.read(intermediaryBuffer, 0, readSize);
             buffer = shortArrayToFloatArray(intermediaryBuffer);
             final float frequency = getPitch(buffer);
@@ -103,12 +145,20 @@ public class MainActivity extends AppCompatActivity {
     private float[] shortArrayToFloatArray(short[] array) {
         float[] fArray = new float[array.length];
         for (int i = 0; i < array.length; i++) {
-            fArray[i] = (float) array[i];
+            fArray[i] = array[i];
         }
         return fArray;
     }
 
-    private native float    getPitch(float[] input);
-    private native void     initPitch(int sampleRate, int B);
-    private native void     cleanupPitch();
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopRecord();
+    }
+
+    private native float getPitch(float[] input);
+
+    private native void initPitch(int sampleRate, int B);
+
+    private native void cleanupPitch();
 }
